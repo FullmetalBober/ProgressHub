276d0337f07a43bfbfb182cd0ab64fb682ed355a
Vladyslav Mankivskyi <mankivskiy.vlsd@gmail.com>
2024-05-12 20:37:21 +0300

    chore: SignOut replace arrow func with func

--- components/auth/SignOut.tsx
+++ components/auth/SignOut.tsx
@@ -3,25 +3,21 @@
 import { signOut } from 'next-auth/react';
 import { forwardRef } from 'react';
 
-const SignOutBtn = forwardRef(
-  (
-    props: React.ButtonHTMLAttributes<HTMLButtonElement>,
-    ref: React.ForwardedRef<HTMLButtonElement>
-  ) => {
-    return (
-      <button
-        {...props}
-        ref={ref}
-        onClick={() =>
-          signOut({
-            callbackUrl: '/',
-          })
-        }
-      >
-        Log out
-      </button>
-    );
-  }
-);
-
-export default SignOutBtn;
+export default forwardRef(function SignOutBtn(
+  props: React.ButtonHTMLAttributes<HTMLButtonElement>,
+  ref: React.ForwardedRef<HTMLButtonElement>
+) {
+  return (
+    <button
+      {...props}
+      ref={ref}
+      onClick={() =>
+        signOut({
+          callbackUrl: '/',
+        })
+      }
+    >
+      Log out
+    </button>
+  );
+});
