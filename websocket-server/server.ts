2b1e4b8719f9789b87ab9fa597fd01e27b60f6a9
Vladyslav Mankivskyi <mankivskiy.vlsd@gmail.com>
2024-06-12 17:40:46 +0300

    set port for hocuspocus

--- websocket-server/server.ts
+++ websocket-server/server.ts
@@ -5,8 +5,8 @@ import { Server } from '@hocuspocus/server';
 import jsonwebtoken from 'jsonwebtoken';
 
 const server = Server.configure({
-  port: 1234,
-  address: '127.0.0.1',
+  port: Number(process.env.PORT) ?? 1324,
+  // address: '127.0.0.1',
   // name: 'hocuspocus-fra1-01',
   extensions: [
     new Logger(),
