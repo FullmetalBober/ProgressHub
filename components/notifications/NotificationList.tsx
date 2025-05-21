ac749fcd77fa827325da7ac8a68413968969d0ca
Vladyslav Mankivskyi <mankivskiy.vlsd@gmail.com>
2025-05-22 01:56:59 +0300

    feat: mark all as read notification button

--- components/notifications/NotificationList.tsx
+++ components/notifications/NotificationList.tsx
@@ -1,7 +1,10 @@
 'use client';
 
 import { useSocketObserver } from '@/hooks/useSocketObserver';
+import { markAllNotificationsAsRead } from '@/lib/actions/notifications.action';
 import { Issue, Notification, User } from '@prisma/client';
+import { useState } from 'react';
+import { Button } from '../ui/button';
 import NotificationItem from './NotificationItem';
 
 export default function NotificationList({
@@ -15,6 +18,7 @@ export default function NotificationList({
     sender: Pick<User, 'id' | 'name' | 'email' | 'image'>;
   })[];
 }) {
+  const [isMarkingAsRead, setIsMarkingAsRead] = useState(false);
   const notificationsObserved = useSocketObserver(
     'notification',
     notifications
@@ -23,17 +27,42 @@ export default function NotificationList({
     (a, b) => b.updatedAt.getTime() - a.updatedAt.getTime()
   );
 
+  const handleMarkAllAsRead = async () => {
+    setIsMarkingAsRead(true);
+
+    await markAllNotificationsAsRead();
+
+    setIsMarkingAsRead(false);
+  };
+
+  const markAllAsReadButtonDisabled =
+    notificationSorted.length === 0 ||
+    notificationSorted.every(notification => notification.isRead) ||
+    isMarkingAsRead;
   return (
-    <div className='flex flex-col gap-2'>
-      {notificationSorted.map(notification => {
-        if (notification.issue !== null)
-          return (
-            <NotificationItem
-              key={notification.id}
-              notification={{ ...notification, issue: notification.issue }}
-            />
-          );
-      })}
+    <div className='flex flex-col gap-4'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-2xl font-bold'>Notifications</h1>
+        <Button
+          variant='outline'
+          size='sm'
+          onClick={handleMarkAllAsRead}
+          disabled={markAllAsReadButtonDisabled}
+        >
+          Mark all as read
+        </Button>
+      </div>
+      <div className='flex flex-col gap-2'>
+        {notificationSorted.map(notification => {
+          if (notification.issue !== null)
+            return (
+              <NotificationItem
+                key={notification.id}
+                notification={{ ...notification, issue: notification.issue }}
+              />
+            );
+        })}
+      </div>
     </div>
   );
 }
